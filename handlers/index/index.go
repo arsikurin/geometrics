7008e49a72bcc16e6e8f1d8836d1e725adfea72f
arsikurin <2024kurin.av@student.letovo.ru>
2022-10-18 13:00:15 +0300

    index now shows problems in courses

--- handlers/index/index.go
+++ handlers/index/index.go
@@ -15,17 +15,15 @@ import (
 func GETIndex(ctx context.Context) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		courses, err := models.Courses(
-			// Select(models.CourseColumns.Name, models.CourseColumns.Description, models.CourseColumns.AuthorID),
 			Load("CoursesProblems"),
 		).AllG(ctx)
 		if err != nil {
 			return errors.WithMessage(err, "get courses failed in get index")
 		}
-		// fmt.Println(courses[0].Author().OneG(ctx))
-		// fmt.Println(courses[0].R.CoursesProblems[1].Problem().OneG(ctx))
 
 		return c.Render(http.StatusOK, "index.gohtml", map[string]interface{}{ //nolint:wrapcheck
 			"courses": courses,
+			"ctx":     ctx,
 		})
 	}
 }
